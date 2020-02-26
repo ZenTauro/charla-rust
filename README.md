366adbf58ba794ddc1e0bf5d786769436fdd526e
zentauro <pedrogomezmartin1999@gmail.com>
2020-02-26 12:19:07 +0100

    Readme, rename y fix

---
+++ README.md
@@ -0,0 +1,6 @@
+# Diapositivas y ejemplos de la charla
+Aquí están todos los ejemplos y las diapositivas.
+
+# Notas
+ - La charla está basada en esta [Curso de rust en tres días](https://github.com/ferrous-systems/rust-three-days-course)
+ - Las diapositivas están hechas con [revealjs](https://revealjs.com/#/)
--- test/test.rs
+++ test/test.rs
@@ -6,17 +6,17 @@ enum Mensaje {
 }
 
 enum Option<T> {
-    Ok(T),
+    Some(T),
     None
 }
 
 fn main() {
-    let mut mensaje = Mensaje::Escribir("Hola mundo");
+    let mut mensaje = Mensaje::Escribir("Hola mundo".to_owned());
     mensaje = Mensaje::Leer;
 
     match mensaje {
-        Mensaje::Escribir(msg) => ...,
-        Mensaje::Leer => ...,
-        Mensaje::Ping => pong(),
+        Mensaje::Escribir(msg) => unimplemented!(),
+        Mensaje::Leer => unimplemented!(),
+        Mensaje::Ping => unimplemented!(),
     }
 }
